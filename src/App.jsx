c20ce3927314eff7e43df7f31bcac0c84e31cb4e
Yaroslav Bozhak <bozhakyaroslav@gmail.com>
2024-12-10 22:55:42 +0000

    created app, added header

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/header/Navbar";
+
+function App() {
+  return (
+    <div className="dark:bg-blue-200">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
